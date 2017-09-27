f969435b00417e5d0b3ef1412403197e00c7854f
jmp9c <jmp9cmtsu@gmail.com>
2017-09-27 00:08:01 -0500

    Initial Commit

---
+++ DriveSync/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DriveSync
+//
+//  Created by John Peden on 9/27/17.
+//  Copyright © 2017 John Peden. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DriveSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DriveSync
+//
+//  Created by John Peden on 9/27/17.
+//  Copyright © 2017 John Peden. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
